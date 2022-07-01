cc92ba6296bb7b3ef35ed2cbf34147e1389055f2
Pushkar Singh <stoazest@gmail.com>
2022-07-01 12:21:29 +0530

    Home Screen Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+// importing screens
+import HomeScreen from "./screens/HomeScreen";
 
 function App(){
     return(
         <div className="App">
-            <h1>Hello World!</h1>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomeScreen />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomeScreen(){
+    return(
+        <div className="screen">
+            <h1>Hey, I am Pushkar Singh</h1>
+        </div>
+    )
+}
+
+export default HomeScreen
